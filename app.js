e838adabd4a49294e87cc2442b754212d4182a55
Yurii <misiailo.yurii@gmail.com>
2024-03-14 18:56:00 +0200

    Removed 'cors' middleware

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 import express from 'express';
-import dotenv from 'dotenv/config';
+import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import { registerValidation } from './validations/auth.js'
-import checkAuth from './utils/checkAuth.js';
-import { register, login, getMe } from './controllers/UserController.js'
+import { register, login, logout, getMe } from './controllers/UserController.js'
 import { createRoute, getAllRoutes, getById, getMyRoutes, removeRoute, updateRoute } from './controllers/RouteControllers.js'
 import { addNextShift, getAllNexts } from './controllers/nextShiftController.js';
 import cookieParser from 'cookie-parser';
-
+import { protect } from './middlewares/authMiddleware.js'
 
 const app = express();
 // middlewares
-app.use(cors())
+// app.use(cors({ credentials: true, origin: 'http://localhost:5173' }))
+// app.use(cors({ credentials: true, origin: ['http://localhost:5173'] }))
 app.use(express.json())
 app.use(cookieParser())
 dotenv.config({ path: './.env' })
 //variables
-// const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5006;
+const MONGO_URL = process.env.MONGO_URL
 
 // const db_password = process.env.db_password
 //*****************************************/
 function InitDB() {
    try {
-      mongoose.connect(`mongodb+srv://misiailoyurii:${db_password}@cluster0.idtz4it.mongodb.net/new_routes?retryWrites=true&w=majority&appName=Cluster0`)
+      mongoose.connect(`${MONGO_URL}`)
       console.log(`Your DB is successfully connected`)
    } catch (error) {
       console.log(error)
@@ -34,17 +35,18 @@ InitDB()
 // Register, Login, GetMe
 app.post('/auth/register', registerValidation, register)
 app.post('/auth/login', login)
-app.get('/auth/me', checkAuth, getMe)
+app.post('/auth/logout', logout)
+app.get('/auth/me', protect, getMe)
 
 // CRUD routes
 app.get('/routes', getAllRoutes)
 app.get('/routes/:id', getById)
-app.post('/routes', checkAuth, createRoute)
-app.delete('/routes/:id', checkAuth, removeRoute)
-app.patch('/routes', checkAuth, updateRoute)
+app.post('/routes', protect, createRoute)
+app.delete('/routes/:id', protect, removeRoute)
+app.patch('/routes', protect, updateRoute)
 // next shift feature
-app.post('/next', checkAuth, addNextShift)
-app.get('/next', checkAuth, getAllNexts)
+app.post('/next', protect, addNextShift)
+app.get('/next', protect, getAllNexts)
 
 app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`)
