4f5c4d9bca5794b890a4a6143354b37624c1f807
Yurii <misiailo.yurii@gmail.com>
2024-03-14 18:54:26 +0200

    Added JWT auth

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,33 +5,38 @@ import { generateToken } from '../utils/generateToken.js'
 
 import { validationResult } from 'express-validator'
 
+// @desc Auth a user
+// @route POST /auth/login
 export const register = async (req, res) => {
+   const { email, name, password } = req.body;
    try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
          return res.status(400).json(errors.array())
       }
-      const { email, name, password } = req.body;
-      const salt = await bcrypt.genSalt(10);
-      const hash = await bcrypt.hash(password, salt);
-      const doc = new UserModel({
-         email,
-         name,
-         avatarUrl: req.body.avatarUrl,
-         passwordHash: hash
+      const userExists = await UserModel.findOne({ email })
+
+      if (userExists) {
+         res.status(400).json({ message: 'Такий користувач вже існує' })
+      }
+
+
+      const user = await UserModel.create({
+         name, email, password
       })
-      const user = await doc.save()
-      // const secret = process.env.jwt_secret
-      const secret = 'uiCKlEAvERfo'
-      const token = jwt.sign({
-         _id: user._id
-      }, secret, { expiresIn: '30d' })
+      if (user) {
+         generateToken(res, user._id)
+         res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+         })
+      }
 
-      const { passwordHash, ...userData } = user._doc;
 
-      res.json({ ...userData, token })
    } catch (error) {
-      res.json({ message: "Не вдалося зареєструватися" })
+      console.log(error)
+      res.json({ message: "Не вдалося зареєструватися", error })
    }
 
 }
@@ -57,13 +62,22 @@ export const login = async (req, res) => {
       console.log(error)
    }
 }
+export const logout = async (req, res) => {
+   try {
+      const { email, password } = req.body;
+
+   } catch (error) {
+      res.json({ error: "Не вдалося ввійти", error })
+      console.log(error)
+   }
+}
 
 export const getMe = async (req, res) => {
    try {
       const user = await UserModel.findById(req.userId)
       if (!user) return res.status(404).json({ message: "Користувача не знайдено" })
 
-      const { passwordHash, ...userData } = user._doc;
+      const { ...userData } = user._doc;
       res.json({ ...userData })
 
    } catch (error) {
